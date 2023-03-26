7803c1d31b7a482d28c264fc2a0b61b780fd2ac7
7894555 <125806909+7894555@users.noreply.github.com>
2023-03-26 17:32:16 +0300

    Add files via upload

--- js/them.js
+++ js/them.js
@@ -2,17 +2,16 @@ let theme =document.querySelector('.themebutton')
 theme.addEventListener("click",function(){
  document.body.classList.toggle("dark");
 })
-
+/*
 let post = document.querySelector('.post');
 let more = post.querySelector('.more');
-more.onclick=function(){
+more.onclick = function(){
     alert("!");
     post.classList.remove('short');
-}
-
-
-
-
+}*/
+var date = new Date();
+document.body.style.backgroundColor= "rgb("+date.getUTCHours()*10.6+","+date.getMinutes()*4.25+","+date.getDay()* 8.5+")";   
+  alert(date.getHours());
 
 
 
